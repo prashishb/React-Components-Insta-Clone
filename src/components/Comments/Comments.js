8f6cacd86de06bda90f222c10bd05522bc337f7e
prashishb <prashb.web@gmail.com>
2021-11-09 21:54:10 -0500

    comments

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Comment from './Comment';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSmile } from '@fortawesome/free-regular-svg-icons';
 import './Comments.css';
 
 const Comments = props => {
@@ -10,6 +12,11 @@ const Comments = props => {
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
       {comments.map(elem => <Comment comment={elem} key={elem.id} />)}
+      <div class="comment-wrapper">
+        <FontAwesomeIcon icon={faSmile} class="smile-icon" />
+        <input type='text' class="comment-box" placeholder="Add a comment"></input>
+        <button class="comment-btn">post</button>
+      </div>
     </div>
   );
 };
